b0023bc679a4819d84f98aac92c7f725b18b6f3f
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2021-11-01 09:56:27 +0100

    Release 0.0.12 with fixes for merge and split commands

--- frontend/src/components/tokens/actions/SplitAction.tsx
+++ frontend/src/components/tokens/actions/SplitAction.tsx
@@ -2,7 +2,7 @@ import {SplitCellsOutlined} from "@ant-design/icons";
 import {Button, Popover} from "antd";
 import React, {useCallback, useState} from "react";
 import {executeCommands} from "../commands/CommandExecutor";
-import {SplitSegmentCommand} from "../commands/Commands";
+import {SplitSegmentCommand, StartCommand, StopCommand} from "../commands/Commands";
 import {TokenOverviewData} from "../TokenOverviewData";
 
 export function SplitAction({row}: { row: TokenOverviewData }) {
@@ -10,17 +10,23 @@ export function SplitAction({row}: { row: TokenOverviewData }) {
 
     const onSplitAction = useCallback(async () => {
         setLoading(true)
+        const chosenNode = row.allNodes[0];
+        const nodesToStop = row.allNodes.filter(node => node != chosenNode);
         await executeCommands([
-                new SplitSegmentCommand(row.owner, row.processorName, row.segment)
+                ...nodesToStop.map(node => new StopCommand(node, row.processorName)),
+                new SplitSegmentCommand(chosenNode, row.processorName, row.segment),
+                ...nodesToStop.map(node => new StartCommand(node, row.processorName)),
             ]
         )
         setLoading(false)
-    }, [row.processorName, row.segment, row.owner])
+    }, [row.processorName, row.segment, row.owner, row.allNodes])
 
     return <Popover
-        content={<p>Splits the token of this segment in two, increasing active threads of the processor by one. <br/>This action needs a free processing thread on the same node and is disabled when there are none.</p>}
+        content={<p>Splits the token of this segment in two, increasing active threads of the processor by one. <br/>This
+            action needs a free processing thread on the same node and is disabled when there are none.</p>}
         placement={"bottom"}>
-        <Button type="default" loading={loading} onClick={onSplitAction} disabled={row.owner == null || !row.threadsAvailable}>
+        <Button type="default" loading={loading} onClick={onSplitAction}
+                disabled={row.owner == null || !row.threadsAvailable}>
             <SplitCellsOutlined/>
         </Button>
     </Popover>
